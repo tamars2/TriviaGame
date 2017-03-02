d0252b2bd06c595320a8e3fd29b4b4423e756e66
Trent Marshall <Trent@lawn-128-61-52-30.lawn.gatech.edu>
2017-03-02 18:55:06 -0500

    v1.0

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,12 @@
 //global variables
 var title = ['M', 'U', '/', 'T', 'H', '/', 'U', 'R', ':', ' ', 'A', 'l', 'i', 'e', 'n', ' ', 'T', 'r', 'i', 'v', 'i', 'a'];
 console.log(title);
-var timer = 60,
+var timer,
 	correct = 0,
 	incorrect = 0,
 	userGuess,
+	myVar,
 	questionIndex = 0;
-
-
-//assign questions and answers to arrays within questions object
-// var questions = {
-// 	q1: ['a', 'b', 'c', 'd', 'e'],
-// 	q2: ['f', 'g', 'h', 'i', 'j'],
-// };
 
 //trivia questions library
 var questions = [
@@ -95,47 +89,35 @@ var buttonClick = new Audio('./assets/javascript/alien_danger.wav');
 $('button').click(function() {
   $(this).slideUp();
   buttonClick.play();
-  //$('#instructions').empty();
-  //$('.radios').show();
+  timer = 45;
   $('#instructions').show();
   $('#time').show();
   loadQuestion();
-  
-
-  //$('#question1').html(questions.q1[0]);
-  
-  // for (var i=1; i<questions.q1.length, i++){
-  // 	$('label[for=test1]').html(questions.q1[i]);
-
-  //$('#question2').html(questions.q2);
-  //setInterval(pulse, 0);
 });
 
 $('.radios').click(function(){
-			//get id for radio button selected by user
-			userGuess = $(this).attr('id');
-			console.log(userGuess);
-			console.log(questions[questionIndex].correctAnswer);
-			//check for correct answer
-			if (userGuess == questions[questionIndex].correctAnswer) {
-				correct = correct + 1;
-				console.log(correct + " correct");
-			}
-			else {
-				incorrect = incorrect + 1;
-				console.log(incorrect + " incorrect");
-			}
-			questionIndex++;
-			checkWin();
-			// if (questionIndex === 5) {
-			// console.log("game over man, game over!");
-			// }
-
-			loadQuestion();
-			$('.radios').prop('checked', false);
+	//get id for radio button selected by user
+	userGuess = $(this).attr('id');
+	console.log(userGuess);
+	console.log(questions[questionIndex].correctAnswer);
+	//check for correct answer
+	if (userGuess == questions[questionIndex].correctAnswer) {
+		correct = correct + 1;
+		console.log(correct + " correct");
+		}
+	else {
+		incorrect = incorrect + 1;
+		console.log(incorrect + " incorrect");
+		}
+		questionIndex++;
+		checkWin();
+		loadQuestion();
+		//clear radio buttons
+		$('.radios').prop('checked', false);
 		});
 
 //loop to print title indexes as game title, emulating typing letters to a CRT monitor
+//useless animation?  maybe.
 for (var i = 0; i < title.length; i++) {
     (function(i){
         setTimeout(function(){
@@ -145,6 +127,7 @@ for (var i = 0; i < title.length; i++) {
     }(i));
 }
 
+//check to see if 3 questions are correctly answered
 function checkWin() {
 	if (correct > 2) {
 		$('.gameboard').hide();
@@ -152,12 +135,11 @@ function checkWin() {
 		$('#result').pulse();
 	}
 	else if (incorrect > 2) {
-		$('.body').html('<img src="./assets/javascript/NostromoExplodes.gif" width="100%"></img>');
+		youLose();
 	}
 };
 //wait for title to load, then show button
 window.setTimeout(function () {
-    //$('#instructions').html('Press Start to begin!');
     $('button').show();
 }, 3400);
 
@@ -166,17 +148,26 @@ function pulse() {
     $('#result').fadeIn(300);
     $('#result').fadeOut(500);
 }
+//inteval for pulsing winning statement
 setInterval(pulse, 1000);
 
-// $(document).ready(function() {
-//     setInterval ('cursorAnimation()', 600);
-//     captionEl = $('#caption');
-//     });
+//start reducing time remaining
+function setTimer() {
+	timer--;
+	checkTime();
+	$('#time').html('Time Remaining: ' + timer);
+}
 
-// function cursorAnimation() {
-//     $('#cursor').animate({
-//         opacity: 0
-//     }, 'fast', 'swing').animate({
-//         opacity: 1
-//     }, 'fast', 'swing');
-// }
+//decrement timer one second every 10000 miliseconds
+setInterval(setTimer, 1000);
+
+//check to see if time has run out
+function checkTime() {
+	if (timer < 0) {
+		youLose();
+	}
+};
+//Game over man, game over!
+function youLose() {
+	$('.body').html('<img src="./assets/javascript/NostromoExplodes.gif" width="100%"></img>');
+}
